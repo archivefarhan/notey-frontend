6b65a32e947fcf33824eb3538c40b38c871bd57e
farhan <farhanj.mohamed@gmail.com>
2023-03-19 14:46:38 -0700

    added login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About } from "./About";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Home } from "./Home";
+import { Login } from "./Login";
 import { Notes } from "./Notes";
 import { Signup } from "./Signup";
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/notes" element={<Notes />} />
           <Route path="/about" element={<About />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
       <Footer />

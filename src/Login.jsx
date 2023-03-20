b5ae47d3f4b8407cf59564745799c2b0553dcf34
farhan <farhanj.mohamed@gmail.com>
2023-03-20 15:30:22 -0700

    addeds stylization to login and sign up

--- src/Login.jsx
+++ src/Login.jsx
@@ -29,22 +29,54 @@ export function Login() {
   };
 
   return (
-    <div id="login">
-      <h1>Login</h1>
-      <ul>
+    <div id="login" className="w-screen h-auto min-h-screen">
+      <h1 className="mt-96 mb-5 text-center font-bold text-4xl">Login</h1>
+      <ul className="text-center mt-5">
         {errors.map((error) => (
           <li key={error}>{error}</li>
         ))}
       </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <div className="text-center">
+        <form onSubmit={handleSubmit}>
+          <label
+            for="UserEmail"
+            class="mt-10 w-1/5 mx-auto relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <input
+              type="email"
+              id="UserEmail"
+              name="email"
+              placeholder="Email"
+              class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+
+            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+              Email
+            </span>
+          </label>
+
+          <label
+            for="Password"
+            class="mt-3 w-1/5 mx-auto relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+
+            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+              Password
+            </span>
+          </label>
+
+          <button className="mt-4 rounded-lg bg-blue-600 text-white w-16 text-center" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -13,7 +13,7 @@ export function Signup() {
       .then((response) => {
         console.log(response.data);
         event.target.reset();
-        window.location.href = "/";
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
       .catch((error) => {
         console.log(error.response.data.errors);
@@ -22,28 +22,89 @@ export function Signup() {
   };
 
   return (
-    <div id="signup">
-      <h1>Signup</h1>
-      <ul>
+    <div id="signup" className="w-screen h-auto min-h-screen">
+      <h1 className="text-center font-bold text-4xl mt-96 mb-5">Signup</h1>
+      <br />
+      <ul className="text-center">
         {errors.map((error) => (
           <li key={error}>{error}</li>
         ))}
       </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input name="name" type="text" />
-        </div>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <div>
-          Password confirmation: <input name="password_confirmation" type="password" />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
+      <div className="text-center">
+        <form onSubmit={handleSubmit} className="w-1/5 mx-auto">
+          <label
+            for="name"
+            class="mt-2 relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              name="name"
+              class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+
+            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+              Name
+            </span>
+          </label>
+
+          <label
+            for="Email"
+            class="mt-2 relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              name="email"
+              class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+
+            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+              Email
+            </span>
+          </label>
+
+          <label
+            for="password"
+            class="mt-2 relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              name="password"
+              class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+
+            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+              Password
+            </span>
+          </label>
+
+          <label
+            for="password_confirmation"
+            class="mt-2 relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+          >
+            <input
+              type="password"
+              id="password_confirmation"
+              placeholder="Password Confirmation"
+              name="password_confirmation"
+              class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            />
+
+            <span class="absolute left-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+              Password Confirmation
+            </span>
+          </label>
+
+          <button type="submit" className="bg-blue-600 rounded-md w-20 text-center text-white mt-5">
+            Signup
+          </button>
+        </form>
+      </div>
     </div>
   );
 }

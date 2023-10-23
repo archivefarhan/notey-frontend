356df9cb38b33edb252d12e885cfd5991157f212
farhan <farhanj.mohamed@gmail.com>
2023-10-23 02:32:26 -0700

    figuring out functionality but added buttons for edit and delete

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -6,6 +6,7 @@ import { LogoutLink } from "./Logout";
 export function Notes() {
   const [openText, setOpenText] = useState(false);
   const [openNote, setOpenNote] = useState(false);
+  const [editNote, setEditNote] = useState(false);
 
   const [notes, setNotes] = useState([]);
 
@@ -23,6 +24,14 @@ export function Notes() {
       window.location.href = "/";
       console.log(response.data);
     });
+  };
+
+  const handleUpdateNote = () => {
+    console.log("working on this");
+  };
+
+  const handleDeleteNote = () => {
+    console.log("working on this");
   };
 
   useEffect(handleIndexNotes, []);
@@ -48,40 +57,51 @@ export function Notes() {
                   <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Notes</span>
-                {localStorage.jwt === undefined ? (
-                  <></>
-                ) : (
-                  <>
-                    <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                      {notes.length}
-                    </span>
-                  </>
-                )}
+
+                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                  {notes.length}
+                </span>
               </a>
             </li>
+
             {localStorage.jwt === undefined ? (
               <>
-                <p>
-                  {" "}
-                  <li>
-                    <a href="/login" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 ">
-                      <svg
-                        aria-hidden="true"
-                        class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                      <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-                    </a>
-                  </li>
-                </p>
+                <li>
+                  <a href="/login" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 ">
+                    <svg
+                      aria-hidden="true"
+                      class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/signup" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 ">
+                    <svg
+                      aria-hidden="true"
+                      className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+                  </a>
+                </li>
               </>
             ) : (
               <>
@@ -107,25 +127,6 @@ export function Notes() {
                 </li>
               </>
             )}
-
-            <li>
-              <a href="/signup" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 ">
-                <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-              </a>
-            </li>
           </ul>
         </div>
       </aside>
@@ -133,7 +134,7 @@ export function Notes() {
       <div className="p-4 sm:ml-64">
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 h-screen">
           {localStorage.jwt === undefined ? (
-            <p className="text-center mt-20">Login to view notes</p>
+            <p className="text-center mt-20 text-4xl font-bold">LOGIN TO VIEW NOTES</p>
           ) : (
             <>
               <div className="mx-auto">
@@ -161,27 +162,58 @@ export function Notes() {
                   </form>
                 ) : (
                   <>
-                    <div className="flex justify-center">
-                      <button
-                        className="mx-auto text-center m-2 text-blue-600 font-bold border border-blue-600 rounded-xl w-1/2"
-                        onClick={() => setOpenText(true)}
-                      >
-                        Add Note
-                      </button>
-                    </div>
-                    <div className="flex justify-center">
-                      <button className="mx-auto text-center m-2 text-orange-400 font-bold border border-orange-400 rounded-xl w-1/2">
-                        Edit Notes
-                      </button>
-                    </div>
-                    <div className="flex justify-center">
-                      <button className="mx-auto text-center m-2 text-red-600 font-bold border border-red-600 rounded-xl w-1/2">
-                        Delete Notes
-                      </button>
-                    </div>
+                    {editNote ? (
+                      <>
+                        <form className="mx-auto text-center m-2" onSubmit={handleUpdateNote}>
+                          <input
+                            placeholder="Title"
+                            className="w-full rounded-lg border border-orange-400 text-sm p-3 mb-2"
+                            name="name"
+                          />
+                          <textarea
+                            className="w-full rounded-lg border-orange-400 p-3 text-sm"
+                            placeholder="Note"
+                            rows="8"
+                            name="body"
+                          ></textarea>
+                          <button className="rounded-lg w-16 p-1 bg-orange-400 text-white mb-2 mr-1">Submit</button>
+
+                          <button
+                            className="rounded-lg w-16 p-1 ml-1 bg-orange-400 text-white mb-2"
+                            onClick={() => setEditNote(false)}
+                          >
+                            Cancel
+                          </button>
+                        </form>
+                      </>
+                    ) : (
+                      <>
+                        <div className="grid grid-cols-3 mb-5 gap-4 ">
+                          <button
+                            className="mx-auto text-center m-2 text-blue-600 font-bold border border-blue-600 rounded-xl w-full"
+                            onClick={() => setOpenText(true)}
+                          >
+                            Add Note
+                          </button>
+                          <button
+                            className="mx-auto text-center m-2 text-orange-400 font-bold border border-orange-400 rounded-xl w-full"
+                            onClick={() => setEditNote(true)}
+                          >
+                            Edit Notes
+                          </button>
+                          <button
+                            className="mx-auto text-center m-2 text-red-600 font-bold border border-red-600 rounded-xl w-full"
+                            onClick={handleDeleteNote}
+                          >
+                            Delete Notes
+                          </button>
+                        </div>
+                      </>
+                    )}
                   </>
                 )}
               </div>
+
               {notes.map((note) => (
                 <div
                   key={note.id}
